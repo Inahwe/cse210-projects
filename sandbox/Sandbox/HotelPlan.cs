54024132b745ce2c52b47b3f43140f059a45fb7e
Inahwe <willieinah1@gmail.com>
2024-05-04 23:06:52 +0100

    Journal02

---
+++ sandbox/Sandbox/HotelPlan.cs
@@ -0,0 +1,16 @@
+
+public class HotelPlan
+{
+    // properties - variables
+    public string _receptionHall;
+    public string _room1;
+    public string _room2;
+
+    // methods (behavior) - funtions
+    public bool OpenDoor(string roomName)
+    {
+        // Console.WriteLine(roomName);
+        bool isOpen = true;
+        return isOpen;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        HotelPlan inahHotel = new HotelPlan();
+        inahHotel._receptionHall = "Main Hall";
+        inahHotel._room1 = "Cambodia Room";
+        inahHotel._room2 = "Guest Room";
+
+        // Console.WriteLine(inahHotel._room2);
+        bool isOpen = inahHotel.OpenDoor(inahHotel._room1);
+        Console.WriteLine(isOpen);
     }
 }
